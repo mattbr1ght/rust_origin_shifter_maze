96e1d33806cfc660368fe3aa50ffc48d7bb72ca9
mattBr1ght <mateuszwronski.yt@gmail.com>
2024-11-18 20:44:20 +0100

    added dfs for path finding and switches for rendering

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@ use rand::distributions::{Distribution, Uniform};
 
 const MAP_WIDTH: usize = 64;
 const MAP_HEIGHT: usize = 24;
+// const MAP_WIDTH: usize = 8;
+// const MAP_HEIGHT: usize = 8;
 static mut NEIGHBOURS: [[bool; 4]; MAP_WIDTH*MAP_HEIGHT] = [[false; 4]; MAP_WIDTH*MAP_HEIGHT];
+static mut OPATH: Vec<usize> = Vec::new();
+static mut PATH: [bool; MAP_WIDTH*MAP_HEIGHT] = [false; MAP_WIDTH*MAP_HEIGHT];
+static mut VISITED: [bool; MAP_WIDTH*MAP_HEIGHT] = [false; MAP_WIDTH*MAP_HEIGHT];
 static mut ORIGIN: usize = MAP_WIDTH*MAP_HEIGHT-1;
 static mut PLAYER: usize = 0;
 const FINISH: usize = MAP_WIDTH*MAP_HEIGHT-1;
+
+static mut ENABLE_ORIGIN: bool = true;
+static mut ENABLE_FINISH: bool = true;
+static mut ENABLE_PATH: bool = true;
+static mut ENABLE_NODE_NUMBERS: bool = false;
 
 use console::Term;
 
@@ -13,15 +23,58 @@ const KEY_W: char = 'w';
 const KEY_S: char = 's';
 const KEY_A: char = 'a';
 const KEY_D: char = 'd';
+const KEY_N: char = 'n';
+const KEY_P: char = 'p';
+const KEY_F: char = 'f';
+const KEY_O: char = 'o';
+
+unsafe fn dfs(a: usize) -> bool{
+    OPATH.push(a);
+    PATH[a] = true;
+    VISITED[a] = true;
+    if a == PLAYER {return true;};
+    for direction in 0..4 {
+        if player_can_move(a, direction){
+            match direction {
+                0 => {
+                    if VISITED[a-1] {continue;}
+                    if !dfs(a-1) {PATH[a-1] = false; OPATH.pop();} else {return true;}
+                    //dfs(a-1);
+                },
+                1 => {
+                    if VISITED[a-MAP_WIDTH] {continue;}
+                    //dfs(a-MAP_WIDTH);
+                    if !dfs(a-MAP_WIDTH) {PATH[a-MAP_WIDTH] = false; OPATH.pop();} else {return true;}
+                },
+                2 => {
+                    if VISITED[a+1] {continue;}
+                    if !dfs(a+1) {PATH[a+1] = false; OPATH.pop();} else {return true;}
+                    //dfs(a+1);
+                },
+                3 => {
+                    if VISITED[a+MAP_WIDTH] {continue;}
+                    if !dfs(a+MAP_WIDTH) {PATH[a+MAP_WIDTH] = false; OPATH.pop();} else {return true;}
+                    //dfs(a+MAP_WIDTH);
+                },
+                _ => {}
+            }
+        }
+    }
+    return false;
+}
 
 unsafe fn pause() {
     let term  = Term::stdout();
     let key = term.read_char();
     match key {
-        Ok(KEY_W) => move_player(1),
-        Ok(KEY_S) => move_player(3),
-        Ok(KEY_A) => move_player(0),
-        Ok(KEY_D) => move_player(2),
+        Ok(KEY_W) => {move_player(1);shift_origin();},
+        Ok(KEY_S) => {move_player(3);shift_origin();},
+        Ok(KEY_A) => {move_player(0);shift_origin();},
+        Ok(KEY_D) => {move_player(2);shift_origin();},
+        Ok(KEY_N) => ENABLE_NODE_NUMBERS = !ENABLE_NODE_NUMBERS,
+        Ok(KEY_P) => ENABLE_PATH = !ENABLE_PATH,
+        Ok(KEY_F) => ENABLE_FINISH = !ENABLE_FINISH,
+        Ok(KEY_O) => ENABLE_ORIGIN = !ENABLE_ORIGIN,
         _ => {}
     }
 }
@@ -33,21 +86,25 @@ fn main() {
             shift_origin();
         }
         loop{
+            OPATH = Vec::new();
+            PATH = [false; MAP_WIDTH*MAP_HEIGHT];
+            VISITED = [false; MAP_WIDTH*MAP_HEIGHT];
+            dfs(FINISH);
             visualize_board();
             if check_win() {print!("HEY! YOU WON!");break;};
             pause();
-            shift_origin();
+            //shift_origin();
         }
     }
 }
 
-unsafe fn player_can_move(direction: usize) -> bool{
-    if can_move(PLAYER, direction) {
+unsafe fn player_can_move(node: usize, direction: usize) -> bool{
+    if can_move(node, direction) {
         match direction{
-            0 => return NEIGHBOURS[PLAYER][0] || NEIGHBOURS[PLAYER-1][2],
-            1 => return NEIGHBOURS[PLAYER][1] || NEIGHBOURS[PLAYER-MAP_WIDTH][3],
-            2 => return NEIGHBOURS[PLAYER][2] || NEIGHBOURS[PLAYER+1][0],
-            3 => return NEIGHBOURS[PLAYER][3] || NEIGHBOURS[PLAYER+MAP_WIDTH][1],
+            0 => return NEIGHBOURS[node][0] || NEIGHBOURS[node-1][2],
+            1 => return NEIGHBOURS[node][1] || NEIGHBOURS[node-MAP_WIDTH][3],
+            2 => return NEIGHBOURS[node][2] || NEIGHBOURS[node+1][0],
+            3 => return NEIGHBOURS[node][3] || NEIGHBOURS[node+MAP_WIDTH][1],
             _ => return false
         }
     }
@@ -59,7 +116,7 @@ unsafe fn check_win() ->  bool{
 }
 
 unsafe fn move_player(direction: usize){
-    if !player_can_move(direction){return;}
+    if !player_can_move(PLAYER, direction){return;}
     match direction {
         0 => PLAYER -= 1,
         1 => PLAYER -= MAP_WIDTH,
@@ -94,8 +151,7 @@ unsafe fn visualize_board(){
         print!("\x1b[41m \x1b[0m");
         for x in 0..MAP_WIDTH {
             let idx: usize = y * MAP_WIDTH + x;
-            //let fmt_idx = format!("{:0>2}", idx);
-            let fmt_idx = format!("{: >2}", "");
+            let fmt_idx = if ENABLE_NODE_NUMBERS {format!("{:0>2}", idx)} else {format!("{: >2}", "")};
             let mut connection = "\x1b[41m \x1b[0m";
             if NEIGHBOURS[idx][2] 
             {connection = " "}
@@ -103,13 +159,15 @@ unsafe fn visualize_board(){
             {connection = " "}
             print!("{}{fmt_idx}{}{connection}", 
                 if idx == PLAYER {"\x1b[42m"}
-                else if idx == ORIGIN {"\x1b[44m"}
-                else if idx == FINISH {"\x1b[45m"}
+                else if idx == ORIGIN && ENABLE_ORIGIN {"\x1b[44m"}
+                else if idx == FINISH && ENABLE_FINISH {"\x1b[45m"}
+                else if PATH[idx] && ENABLE_PATH {"\x1b[43m"}
                 else {""}, 
                 if 
-                   idx == ORIGIN ||
                    idx == PLAYER ||
-                   idx  == FINISH 
+                   (idx == ORIGIN && ENABLE_ORIGIN)||
+                   (idx  == FINISH && ENABLE_FINISH) || 
+                   (PATH[idx] && ENABLE_PATH)
                    {"\x1b[0m"}
                 else {""});
         }
